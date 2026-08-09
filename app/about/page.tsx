a898ca7be99710ca63f009f6caa87845ecbaaf72
davinash97 <avinashd2904@gmail.com>
2026-08-09 22:19:25 +0530

    add contact, about placeholder

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+	return (
+		<div className="flex w-dvw h-dvh items-center justify-center">
+			<div className="flex flex-col gap-4 rounded-xl p-5 w-fit ">
+				<h1 className="w-full text-4xl text-center">About</h1>
+			</div>
+		</div>
+	)
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+	return (
+		<div className="flex w-dvw h-dvh items-center justify-center">
+			<div className="flex flex-col gap-4 rounded-xl p-5 w-fit ">
+				<h1 className="w-full text-4xl text-center">Contact</h1>
+			</div>
+		</div>
+	)
+}
