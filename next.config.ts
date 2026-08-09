dea2e1e87a0b55ee68d26d9b6dff1d603f9f39c1
davinash97 <avinashd2904@gmail.com>
2026-08-09 22:21:59 +0530

    add cors support

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	devIndicators: false
+	async headers() {
+		return [
+			{
+				source: "/:path*",
+				headers: [
+					{ key: "Access-Control-Allow-Credentials", value: "true" },
+					{ key: "Access-Control-Allow-Origin", value: "http://localhost:8080" }, // Change to a specific domain for production
+					{ key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+					{ key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
